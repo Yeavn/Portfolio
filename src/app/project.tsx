92b9b699c6c141d6ae76dae7e2692709daf59df9
Tim S. <pcstoepsel@gmail.com>
2025-06-02 22:40:38 +0200

    update

--- src/app/project.tsx
+++ src/app/project.tsx
@@ -1,16 +1,16 @@
 import { CiLocationArrow1 } from "react-icons/ci";
 
-export default function Project({project, descrtiption, demoLink}: {project?: string, descrtiption?: string, demoLink?: string}) {
+export default function Project({project, img, descrtiption, demoLink}: {project?: string, img?: string, descrtiption?: string, demoLink?: string}) {
     return (
-        <div className="w-full lg:w-1/4 flex items-center justify-center flex-col shadow-lg">
+        <div className="w-full lg:w-2/5 flex items-center justify-center flex-col shadow-lg">
             <div className="w-full bg-[#1E1E1E] py-6 flex items-center justify-center rounded-t-lg">
-                <img src="/icons/placeholder.png" alt="" />
+                <img src={`/projects/${img}`} alt="" className="w-4/5 rounded-lg shadow-lg" />
             </div>
             <div className="bg-[#353535] w-full p-8 rounded-b-lg">
                 <h1 className="text-xl text-[#C7C5C5]">{project}</h1>
                 <p className="text-[#878787] mt-2">{descrtiption}</p>
-                <div className="mt-4"><a className=" text-[#FD6F00]" href={demoLink}><CiLocationArrow1 className="inline mr-2"  />
- Demo ansehen</a></div>
+                {/* <div className="mt-4"><a className=" text-[#FD6F00]" href={demoLink}><CiLocationArrow1 className="inline mr-2"  />Demo ansehen</a></div> */}
+                <div className="mt-4"><p className=" text-[#FD6F00]"><CiLocationArrow1 className="inline mr-2"  />Demo folgt</p></div>
             </div>
         </div>
     )
